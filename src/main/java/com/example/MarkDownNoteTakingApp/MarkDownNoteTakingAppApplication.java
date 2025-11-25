d56fd21a3e7276b02ba84fb07d2cd37cee6836a3
namnguyen <namhoanghainguyen@gmail.com>
2025-11-25 15:45:10 -0700

    start new project

---
+++ src/main/java/com/example/MarkDownNoteTakingApp/MarkDownNoteTakingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.MarkDownNoteTakingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkDownNoteTakingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkDownNoteTakingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MarkDownNoteTakingApp/MarkDownNoteTakingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MarkDownNoteTakingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkDownNoteTakingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
