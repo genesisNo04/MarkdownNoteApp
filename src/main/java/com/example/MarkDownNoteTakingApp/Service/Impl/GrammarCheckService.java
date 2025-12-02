b0e0bc22772a06da0cf67837cafd6d685313dff3
namnguyen <namhoanghainguyen@gmail.com>
2025-12-01 17:00:59 -0700

    finish up the project

--- src/main/java/com/example/MarkDownNoteTakingApp/Service/Impl/GrammarCheckService.java
+++ src/main/java/com/example/MarkDownNoteTakingApp/Service/Impl/GrammarCheckService.java
@@ -1,11 +1,9 @@
 package com.example.MarkDownNoteTakingApp.Service.Impl;
 
 import org.languagetool.JLanguageTool;
-import org.languagetool.language.*;
 import org.languagetool.rules.RuleMatch;
-import org.languagetool.Language;
-import org.languagetool.Languages;
 import org.springframework.stereotype.Service;
+import org.languagetool.language.English;
 
 import java.io.IOException;
 import java.util.List;
@@ -16,10 +14,19 @@ public class GrammarCheckService {
     private final JLanguageTool langTool;
 
     public GrammarCheckService() throws IOException {
-        langTool = new JLanguageTool(Languages.getLanguageForShortCode("en-US"));
+        langTool = new JLanguageTool(new English());
     }
 
     public List<RuleMatch> checkGrammar(String text) throws IOException {
+        //run through all enabled rules for english
+        // each rulematch will tell the start and end and the message show what the error is and suggestion
         return langTool.check(text);
+
+        //after the list is return can run like this to get the info out
+//        List<RuleMatch> matches = grammarCheckService.checkGrammar("She dont like it.");
+//        for (RuleMatch match : matches) {
+//            System.out.println(match.getMessage());
+//            System.out.println("Suggestion: " + match.getSuggestedReplacements());
+//        }
     }
 }
--- src/main/java/com/example/MarkDownNoteTakingApp/Service/Impl/MarkdownService.java
+++ src/main/java/com/example/MarkDownNoteTakingApp/Service/Impl/MarkdownService.java
@@ -1,5 +1,6 @@
 package com.example.MarkDownNoteTakingApp.Service.Impl;
 
+//flexmark is a library used to parse the markdown text
 import com.vladsch.flexmark.ext.autolink.AutolinkExtension;
 import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
 import com.vladsch.flexmark.ext.tables.TablesExtension;
@@ -18,20 +19,30 @@ public class MarkdownService {
     private final HtmlRenderer renderer;
 
     public MarkdownService() {
+        //create a config object to set options for parser and renderer.
+        //for flexmark only
         MutableDataSet options = new MutableDataSet();
+        //Parser.EXTENSIONS a key to tell what extension to use
+        //AutolinkExtension: auto converts URL into clickable link
+        //StrikethroughExtension: support strikethrough text using ~~text~~
+        //TablesExtension: support table
         options.set(Parser.EXTENSIONS, Arrays.asList(
                 AutolinkExtension.create(),
                 StrikethroughExtension.create(),
                 TablesExtension.create()
         ));
 
+        //build parser with the options
         this.parser = Parser.builder(options).build();
+        //build renderer with the options
         this.renderer = HtmlRenderer.builder(options).build();
     }
 
     public String renderToHtml(String markdown) {
-
+        //take the input markdown string and parse it into a document tree(node)
+        //Every markdown element(heading, paragraph, list, table) becomes a node in the tree
         Node document = parser.parse(markdown);
+        //convert AST tree into HTML string
         return renderer.render(document);
     }
 }
